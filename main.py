3991ea49a93b46a07c36fd444f493f1929b59e40
イレイナ❤ <69030283+ElainaMoe@users.noreply.github.com>
2022-07-02 08:55:15 +0000

    Log now can be shown in Action

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from selenium.common.exceptions import ElementNotInteractableException, NoSuchEl
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.edge.service import Service as EdgeService
+from utils.Logger import logger
 import requests as r
 from tqdm import tqdm
 
@@ -31,6 +32,8 @@ def download(url: str, fname: str, headers: dict = {}):     # Working with Windo
             bar.update(size)
 
 
+log = logger('INFO', './SteamAutoQueue.log')
+
 if __name__ == '__main__':
     logo = r'''
      __ _                           _         _            ____                       
@@ -40,7 +43,7 @@ if __name__ == '__main__':
     \__/\__\___|\__,_|_| |_| |_| \_/ \_/\__,_|\__\___/  \___,_\ \__,_|\___|\__,_|\___|
 
                                                             -- Made by GamerNoTitle'''
-    print(logo)
+    log.info(logo)
     try:
         redisURL = os.environ.get('REDIS_URL')
         if redisURL != None:
@@ -49,29 +52,30 @@ if __name__ == '__main__':
             sql = redis.Redis(host=host, password=password,
                               port=port, ssl=True)
             cookies = json.loads(sql.get('steamCookie').decode())
-            print('[SteamAutoQueue] Cookie get from Redis')
+            log.info('[SteamAutoQueue] Cookie get from Redis')
             config = {'proxy': ''}
             debug = False
         else:
-            print('[SteamAutoQueue] Redis URL not set.')
-            print('[SteamAutoQueue] We are trying to use the local file config.json')
+            log.warning('[SteamAutoQueue] Redis URL not set.')
+            log.info(
+                '[SteamAutoQueue] We are trying to use the local file config.json')
             if os.path.exists('config.json'):
                 with open('config.json') as file:
                     config = json.load(file)
                     if config['steam'] != {'sessionid': '', 'steamRememberLogin': '', f'steamMachineAuth{config["steam"]["steamID64"]}': '', 'steamLoginSecure': '', 'browserid': ''}:
                         cookies = {'sessionid': config['steam']['sessionid'], 'steamRememberLogin': config['steam']['steamRememberLogin'], f'steamMachineAuth{config["steam"]["steamID64"]}': config['steam']
                                    ['steamMachineAuth'], 'steamLoginSecure': config['steam']['steamLoginSecure'], 'browserid': config['steam']['browserid']}
-                        print(
+                        log.info(
                             '[SteamAutoQueue] Cookie get from local file config.json')
                         debug = False if config['debug'] != True else True
                     else:
-                        print('You need to configure your cookie first!')
+                        log.error('You need to configure your cookie first!')
                         os._exit(0)
             else:
-                print(
+                log.warning(
                     '[SteamAutoQueue] Cannot found local file config.json, we are trying to use environment variable.')
                 if os.environ.get('sessionid') == None or os.environ.get('steamRememberLogin') == None or os.environ.get('steamMachineAuth') == None or os.environ.get('steamLoginSecure') == None or os.environ.get('browserid') == None:
-                    print(
+                    log.error(
                         '[SteamAutoQueue] No information can be found in your system variable. We will now exit.')
                     os._exit(0)
                 else:
@@ -79,10 +83,11 @@ if __name__ == '__main__':
                                f'steamMachineAuth{os.environ.get("steamID64")}': os.environ.get('steamMachineAuth'),
                                'steamLoginSecure': os.environ.get('steamLoginSecure'), 'browserid': os.environ.get('browserid')}
                     config = {'proxy': ''}
-                    print('[SteamAutoQueue] Cookie get from environment variable')
+                    log.info(
+                        '[SteamAutoQueue] Cookie get from environment variable')
                     debug = False
     except Exception as e:
-        print(f'[SteamAutoQueue] Cannot read config with exception {e}')
+        log.error(f'[SteamAutoQueue] Cannot read config with exception {e}')
         os.exit()
     # -ignore-ssl-errors for ignore SSL Errors, or the console will be filled with them
     option = webdriver.ChromeOptions()
@@ -101,89 +106,48 @@ if __name__ == '__main__':
         else:
             option.add_argument(f'headless --ignore-certificate-errors')
     if sys.platform == 'linux':
-        # try:
-        #     print('[SteamAutoQueue] Removing local files if exist.')
-        #     os.system('sudo rm -f chromedriver_linux64.zip')
-        #     os.system('sudo rm chromedriver -f')
-        #     print('[SteamAutoQueue] Removed.')
-        # except:
-        #     print('[SteamAutoQueue] No local file needs to be removed.')
-        # print('[SteamAutoQueue] Downloading chromedriver')
-        # download('https://chromedriver.storage.googleapis.com/102.0.5005.61/chromedriver_linux64.zip', 'chromedriver.zip')
-        # print('[SteamAutoQueue] Unzipping chromedriver.zip')
-        # os.system('unzip chromedriver.zip')
-        # os.system('sudo rm -f "./chromedriver.zip"')
-        # with zipfile.ZipFile('./chromedriver.zip', 'r') as chromedriver:
-        #         chromedriver.extractall(path='.')
-        # print('[SteamAutoQueue] Downloading chromedriver...')
-        # download('https://raw.githubusercontent.com/Vikutorika/assets/master/files/webdriver/chromedriver102.0.5005.61', 'chromedriver')
-        # with open('./chromedriver', 'wb') as file:
-        #     res = r.get('https://raw.githubusercontent.com/Vikutorika/assets/master/files/webdriver/chromedriver102.0.5005.61')
-        #     file.write(res.content)
-        # print('[SteamAutoQueue] Giving permission to execute.')
-        # os.system('sudo chmod +x "chromedriver"')
-        # print('[SteamAutoQueue] Moving chromedriver to /usr/bin')
-        # os.system('sudo cp chromedriver /usr/bin/')
-        # print('[SteamAutoQueue] Installing chrome...')
-        # os.popen('sudo apt install google-chrome-stable -y')
-        # os.popen('sudo apt update && sudo apt install microsoft-edge-stable -y')
-        # os.system('wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_103.0.1264.37-1_amd64.deb && sudo dpkg -i microsoft-edge-stable_103.0.1264.37-1_amd64.deb && sudo rm -rf microsoft-edge-stable_103.0.1264.37-1_amd64.deb')
-        # print('[SteamAutoQueue] Setting chromedriver\'s path...')
-        # path = os.popen('pwd').read()
-        # os.environ["webdriver.chrome.driver"] = f'{path}/chromedriver'
-        # os.environ['webdriver.edge.driver'] = f'{path}/msedgedriver'
-        # os.system('sudo cp msedgedriver /usr/bin')
-        # print('[SteamAutoQueue] Adding edgedriver\'s arguments')
         option = webdriver.ChromeOptions()
-        # option.add_argument('blink-settings=imagesEnabled=false --no-sandbox --disable-gpu --disable-dev-shm-usage --headless')
         option.add_argument('--no-sandbox')
         option.add_argument('--disable-gpu')
         option.add_argument('--disable-dev-shm-usage')
         option.add_argument('--headless')
-        print('[SteamAutoQueue] Initalizing instance...')
-        browser = webdriver.Chrome(executable_path='chromedriver', options=option)
-        # EdgeOptions = {
-        #     "browserName": "MicrosoftEdge",
-        #     "version": "103.0.1264.37",
-        #     "platform": "LINUX",
-        #     "ms:edgeOptions": {
-        #         "extensions": [], "args": ["--start-maximized", "headless", "--disable-gpu-program-cache", "--disable-gpu"]
-        #     }
-        # }
-        # browser = webdriver.Edge('msedgedriver', EdgeOptions, keep_alive=True, log_path='debug.log')
-        print('[SteamAutoQueue] Instance initalized.')
+        log.info('[SteamAutoQueue] Initalizing instance...')
+        browser = webdriver.Chrome(
+            executable_path='chromedriver', options=option)
+        browser.set_page_load_timeout(60)
+        log.info('[SteamAutoQueue] Instance initalized.')
     elif sys.platform == 'win32':
         if not os.path.exists('./driver/chromedriver.exe'):
             download(
                 'https://chromedriver.storage.googleapis.com/102.0.5005.61/chromedriver_win32.zip', 'chromedriver.zip')
-            print('[SteamAutoQueue] Unzipping chromedriver.zip')
+            log.info('[SteamAutoQueue] Unzipping chromedriver.zip')
             with zipfile.ZipFile('./chromedriver.zip', 'r') as chromedriver:
                 chromedriver.extractall(path='./driver')
-            print('[SteamAutoQueue] Deleting chromedriver.zip')
+            log.info('[SteamAutoQueue] Deleting chromedriver.zip')
             os.system('del /q /f "chromedriver.zip"')
-            print('[SteamAutoQueue] Done!')
+            log.info('[SteamAutoQueue] Done!')
         browser = webdriver.Chrome(
             service=Service('./driver/chromedriver.exe'), options=option)
     else:
-        print(f'[SteamAutoQueue] Unsupported platform {sys.platform}')
+        log.error(f'[SteamAutoQueue] Unsupported platform {sys.platform}')
         os._exit(0)
 
     # Browse Steam page
-    print('[SteamAutoQueue] Trying to access steam store.')
+    log.info('[SteamAutoQueue] Trying to access steam store.')
     browser.get('https://store.steampowered.com/')
-    print('[SteamAutoQueue] Successfully access steam store. Adding cookie...')
+    log.info('[SteamAutoQueue] Successfully access steam store. Adding cookie...')
     for i in cookies:   # Must behind of get, or the browser doesn't know which website to add
         # Set cookies to get logging in
         browser.add_cookie(cookie_dict={'name': i, 'value': cookies[i]})
-    print('[SteamAutoQueue] Successfully add cookie.')
+    log.info('[SteamAutoQueue] Successfully add cookie.')
     browser.refresh()
 
     try:
         username = browser.find_element(
             by=By.ID, value='account_pulldown').text
-        print(f'[SteamAutoQueue] You have been logged in as {username}')
+        log.info(f'[SteamAutoQueue] You have been logged in as {username}')
     except Exception as e:
-        print(
+        log.error(
             f'[SteamAutoQueue] It seems that you are not logged in. Excepted: {e}')
         os._exit(0)
 
@@ -192,58 +156,63 @@ if __name__ == '__main__':
     '''
 
     try:
-        print('[SteamAutoQueue] Try to start the queue.')
+        log.info('[SteamAutoQueue] Try to start the queue.')
         # browser.find_element_by_id('discovery_queue_start_link').click()
         browser.find_element(
             by=By.ID, value='discovery_queue_start_link').click()
     # When the user has already explore a queue and not spawn another
     except ElementNotInteractableException or NoSuchElementException:
-        print(
+        log.info(
             '[SteamAutoQueue] Start the queue failed, maybe you have already started a queue.')
-        print('[SteamAutoQueue] We will try to spawn a new one.')
+        log.info('[SteamAutoQueue] We will try to spawn a new one.')
         # browser.find_element_by_id('refresh_queue_btn').click()
         # browser.find_element(
         #     by=By.CLASS_NAME, value='discover_queue_empty_refresh_btn').click()
         browser.get('https://store.steampowered.com/explore/startnew')
     nextQueueCount = 0
     Done = False
-    while nextQueueCount <= 2:     # When the spawn button has been clicked twice
-        if Done: break
-        print(f'[SteamAutoQueue] Starting Queue No.{nextQueueCount+1}')
-        while True:
-            try:
-                # game = browser.find_element_by_id('appHubAppName').text
-                link = browser.current_url
+    try:        # For Action to show log successfully
+        while nextQueueCount <= 2:     # When the spawn button has been clicked twice
+            if Done:
+                break
+            log.info(f'[SteamAutoQueue] Starting Queue No.{nextQueueCount+1}')
+            while True:
                 try:
-                    game = browser.find_element(
-                        by=By.ID, value='appHubAppName').text
-                    print(
-                        f'[SteamAutoQueue] Exploring {game} with link {link}')
-                    # browser.find_element_by_class_name('next_in_queue_content').click()
-                    browser.find_element(
-                        by=By.CLASS_NAME, value='next_in_queue_content').click()
-                except NoSuchElementException:
-                    agecheck = browser.find_element(
-                        by=By.CLASS_NAME, value='agegate_text_container')
-                    if agecheck:
-                        print(
-                            f'[SteamAutoQueue] Found age check when accessing {link}, skipping.')
+                    # game = browser.find_element_by_id('appHubAppName').text
+                    link = browser.current_url
+                    try:
+                        game = browser.find_element(
+                            by=By.ID, value='appHubAppName').text
+                        log.info(
+                            f'[SteamAutoQueue] Exploring {game} with link {link}')
+                        # browser.find_element_by_class_name('next_in_queue_content').click()
                         browser.find_element(
                             by=By.CLASS_NAME, value='next_in_queue_content').click()
-            except:
-                if nextQueueCount != 2:
-                    print(
-                        '[SteamAutoQueue] Queue is empty, trying to spawn a new one.')
-                    # browser.find_element_by_name('refresh_queue_btn').click()
-                    # browser.find_element(
-                    #     by=By.ID, value='refresh_queue_btn').click()
-                    browser.get(
-                        'https://store.steampowered.com/explore/startnew')
-                    print('[SteamAutoQueue] Spawned. Now we will continue the work.')
-                    nextQueueCount += 1
-                else: Done = True
-                break
-
-    print('[SteamAutoQueue] SteamAutoQueue\'s work has done!')
+                    except NoSuchElementException:
+                        agecheck = browser.find_element(
+                            by=By.CLASS_NAME, value='agegate_text_container')
+                        if agecheck:
+                            log.info(
+                                f'[SteamAutoQueue] Found age check when accessing {link}, skipping.')
+                            browser.find_element(
+                                by=By.CLASS_NAME, value='next_in_queue_content').click()
+                except:
+                    if nextQueueCount != 2:
+                        log.info(
+                            '[SteamAutoQueue] Queue is empty, trying to spawn a new one.')
+                        # browser.find_element_by_name('refresh_queue_btn').click()
+                        # browser.find_element(
+                        #     by=By.ID, value='refresh_queue_btn').click()
+                        browser.get(
+                            'https://store.steampowered.com/explore/startnew')
+                        log.info(
+                            '[SteamAutoQueue] Spawned. Now we will continue the work.')
+                        nextQueueCount += 1
+                    else:
+                        Done = True
+                    break
+    except Exception as e:
+        log.error(e)
+    log.info('[SteamAutoQueue] SteamAutoQueue\'s work has done!')
     browser.quit()
     os._exit(0)
